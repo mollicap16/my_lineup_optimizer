2a0ccd3498914d8bc2f8611f5844632c1ce3a02a
mollicap16 <mollicap16@yahoo.com>
2019-09-03 15:34:15 -0400

    Updated Export functionality for basic_dfs_optimizer_example.py and imported new DK salaries data.

--- lineup_optimizer/basic_dfs_optimizer_example.py
+++ lineup_optimizer/basic_dfs_optimizer_example.py
@@ -1,7 +1,8 @@
-from pydfs_lineup_optimizer import get_optimizer, Site, Sport
+from pydfs_lineup_optimizer import get_optimizer, Site, Sport, CSVLineupExporter
 import pandas
 
 csv_player_path="/home/pete/projects/my_lineup_optimizer/data/draft_kings_players_template/DKSalaries.csv"
+lineups_file="/home/pete/projects/my_lineup_optimizer/lineup_optimizer/lineups/results.csv"
 
 optimizer = get_optimizer(Site.DRAFTKINGS, Sport.FOOTBALL)
 optimizer.load_players_from_csv(csv_player_path)
@@ -12,3 +13,5 @@ for lineup in optimizer.optimize(number_of_lineups):
 #    print(lineup.fantasy_points_projection)
 #    print(lineup.salary_costs)
 
+exporter = CSVLineupExporter(optimizer.optimize(number_of_lineups))
+exporter.export(lineups_file)
--- lineup_optimizer/player_efficiency_test.py
+++ lineup_optimizer/player_efficiency_test.py
@@ -5,6 +5,10 @@ csv_player_path="/home/pete/projects/my_lineup_optimizer/data/draft_kings_player
 
 optimizer = get_optimizer(Site.DRAFTKINGS, Sport.FOOTBALL)
 optimizer.load_players_from_csv(csv_player_path)
-for player in optimizer.players:
-    print(player.first_name, " ", player.last_name, ": ", player.efficiency * 100)
+players = optimizer.players
+
+most_efficient_players = sorted(players, key=lambda x: x.efficiency, reverse=True)
+
+for player in most_efficient_players:
+    print(player.first_name, " ", player.last_name, ": ", player.efficiency * 1000)
 
