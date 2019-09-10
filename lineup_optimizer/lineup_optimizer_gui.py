5d14641826929f58967d8bea4f748ef80f9c04d0
mollicap16 <mollicap16@yahoo.com>
2019-09-10 15:57:33 -0400

    Added file dialog to gui.

--- lineup_optimizer/lineup_optimizer_gui.py
+++ lineup_optimizer/lineup_optimizer_gui.py
@@ -1,16 +1,17 @@
 import tkinter as tk
+from tkinter import filedialog
 from pydfs_lineup_optimizer import get_optimizer, Site, Sport 
 
 # Creating Main window object
 main_window = tk.Tk()
 
-load_entry_text="";
-load_entry = tk.Entry(main_window, text="empty", bg='white', relief='groove', state='disable')
-load_entry.grid(column=1, row=0)
+load_label_text="";
+load_label = tk.Label(main_window, text="empty", bg='white', relief='groove', width=50)
+load_label.grid(column=1, row=0)
 
 def load_click():
-    load_entry_text = "Clicked"
-    load_entry.configure(text=load_entry_text)
+    file_load = filedialog.askopenfilename()
+    load_label.configure(text=file_load, width=0)
 
 load_button = tk.Button(main_window, text="Load", command=load_click)
 load_button.grid(column=0, row=0)
