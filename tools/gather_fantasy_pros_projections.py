a83ba17226ffcb6bff5399e259b59fc30fe1cb5f
mollicap16 <mollicap16@yahoo.com>
2019-09-25 22:17:29 -0400

    Removed unnecessary tool.

--- tools/gather_fantasy_pros_projections.py
+++
@@ -1,5 +0,0 @@
-# TODO: Create a script that will download the fantasy pros projections and 
-#       the fantasy football analytics projections
-
-fantasy_pros_url = "https://www.fantasypros.com/nfl/projections/wr.php?week=1&scoring=PPR"
-
