690cd4318b37ffddfed47c0a7f9eb6d7f8e6811b
mollicap16 <mollicap16@yahoo.com>
2019-10-17 21:20:08 -0400

    Added locked and excluded players list.

--- lineup_optimizer/lineup_optimizer_gui.py
+++ lineup_optimizer/lineup_optimizer_gui.py
@@ -33,6 +33,8 @@ def DraftKingsRefromatting(filename):
 player_file = ''
 ffa_player_projections = pd.DataFrame()
 optimizer = get_optimizer(Site.DRAFTKINGS, Sport.FOOTBALL) 
+locked_player_list=[]
+excluded_player_list=[]
 
 # Creating Main window object
 main_window = tk.Tk()
@@ -43,17 +45,28 @@ ttk.Sizegrip(main_window).grid(column=999, row=999, sticky=('SE'))
 # TODO: Might want to remove borderwidth and relief for frames
 # Frames
 top_frame = ttk.Frame(main_window, borderwidth=4, relief='sunken')
-top_frame.grid(column=0, row=0, sticky=('N', 'W', 'E', 'S'))
+top_frame.grid(column=0, row=0, columnspan=2, sticky=('N', 'W', 'E', 'S'))
 
-middle_frame = ttk.Frame(main_window, borderwidth=4, relief='sunken')
-middle_frame.grid(column=0, row=1, sticky=('N', 'W', 'E', 'S'))
+middle_left_frame = ttk.Frame(main_window, borderwidth=4, relief='sunken')
+middle_left_frame.grid(column=0, row=1, sticky=('N', 'W', 'E', 'S'))
+
+middle_right_frame = ttk.Frame(main_window, borderwidth=4, relief='sunken')
+middle_right_frame.grid(column=1, row=1)
 
 bottom_frame = ttk.Frame(main_window, borderwidth=4, relief='sunken')
-bottom_frame.grid(column=0, row=2, sticky=('N', 'W', 'E', 'S'))
+bottom_frame.grid(column=0, row=2, columnspan=2, sticky=('N', 'W', 'E', 'S'))
+
+locked_player_frame = ttk.Frame(middle_right_frame, borderwidth=4, relief='sunken')
+locked_player_frame.grid(column=0, row=0)
+
+excluded_player_frame = ttk.Frame(middle_right_frame, borderwidth=4, relief='sunken')
+excluded_player_frame.grid(column=0, row=1)
 
 # This just tells Tk that if the main window is resized the frame should expand with it
 main_window.columnconfigure(0, weight=1)
 main_window.rowconfigure(1, weight =1)
+
+table = Table(middle_left_frame)
 
 # Button Commands
 def load_click():
@@ -62,6 +75,7 @@ def load_click():
     save_button.state(['disabled'])
     optimize_button.state(['!disabled'])
     try:
+        global ffa_player_projections
         ffa_player_projections = pd.read_csv(player_file)
         LoadOptimizer(player_file)
         player_value = []
@@ -69,11 +83,18 @@ def load_click():
         ffa_player_projections = ffa_player_projections.assign(Value = player_value)
     except:
         sys.exit("Invalid file type")
-    table = Table(middle_frame, dataframe=ffa_player_projections)
+    global table 
+    table = Table(middle_left_frame, dataframe=ffa_player_projections)
     table.grid(column=0, row=0, rowspan=2)
     table.show()
+    add_lock_button.state(['!disabled'])
+    add_exclude_button.state(['!disabled'])
+    remove_lock_button.state(['!disabled'])
+    remove_exclude_button.state(['!disabled'])
+
 
 def optimize_click():
+    # TODO: Add helper functions to add excluded and locked players to the optimizer
 #    excluded_player = optimizer.get_player_by_name('Terry McLaurin')
 #    excluded_player2 = optimizer.get_player_by_name('Devin Singletary')
 #    optimizer.remove_player(excluded_player)
@@ -94,17 +115,63 @@ def save_click():
     DraftKingsRefromatting(results)
     save_button.state(['disabled'])
 
+def add_locked_player_click():
+    row = table.getSelectedRow()
+    player = ffa_player_projections.iloc[row].Name
+    if (player not in locked_player_list and player not in excluded_player_list):
+        locked_player_list.append(player)
+
+    locked_player_listbox.delete(0,'end')
+    for item in locked_player_list:
+        locked_player_listbox.insert('end', item)
+    print(locked_player_list)
+
+def remove_locked_player_click():
+    player = locked_player_listbox.get('active')
+    if (player in locked_player_list):
+        locked_player_list.remove(player)
+
+    locked_player_listbox.delete(0,'end')
+    for item in locked_player_list:
+        locked_player_listbox.insert('end', item)
+    print(locked_player_list)
+
+def add_excluded_player_click():
+    row = table.getSelectedRow()
+    player = ffa_player_projections.iloc[row].Name
+    if (player not in excluded_player_list and player not in locked_player_list):
+        excluded_player_list.append(player)
+
+    excluded_player_listbox.delete(0,'end')
+    for item in excluded_player_list:
+        excluded_player_listbox.insert('end', item)
+    print(excluded_player_list)
+
+def remove_excluded_player_click():
+    player = excluded_player_listbox.get('active')
+    if (player in excluded_player_list):
+        excluded_player_list.remove(player)
+
+    excluded_player_listbox.delete(0,'end')
+    for item in excluded_player_list:
+        excluded_player_listbox.insert('end', item)
+    print(excluded_player_list)
+
 # list box
-#locked_player_list = tk.Listbox(middle_frame)
-#locked_player_list.grid(column = 1, row = 0)
-#excluded_player_list = tk.Listbox(middle_frame)
-#excluded_player_list.grid(column = 1, row = 1)
+locked_player_listbox = tk.Listbox(locked_player_frame, height=10)
+locked_player_listbox.grid(column = 0, row = 1, columnspan=2, sticky=('N', 'W', 'E', 'S'))
+excluded_player_listbox = tk.Listbox(excluded_player_frame)
+excluded_player_listbox.grid(column=0, row=1, columnspan=2, sticky=('N', 'W', 'E', 'S'))
 
 # Labels
 load_label = ttk.Label(top_frame, text="empty", background='white', relief='groove', width=50)
 load_label.grid(column=1, row=0, sticky='E')
 num_lineup_label = ttk.Label(bottom_frame, text="Number of Lineups")
 num_lineup_label.grid(column=3, row=0, sticky='W')
+locked_player_label = ttk.Label(locked_player_frame, text='Locked Players', font='-weight bold')
+locked_player_label.grid(column = 0, row = 0, columnspan=2, sticky=('N', 'W', 'E', 'S'))
+excluded_player_label = ttk.Label(excluded_player_frame, text='Excluded Players', font='-weight bold')
+excluded_player_label.grid(column=0, row=0, columnspan=2, sticky=('N', 'W', 'E', 'S'))
 
 # Entries
 num_lineups = tk.StringVar() 
@@ -121,6 +188,18 @@ optimize_button.grid(column=1, row=0, sticky="E")
 
 save_button = ttk.Button(bottom_frame, text="Save Lineups", command=save_click, state='disabled')
 save_button.grid(column=0, row=0, sticky="W")
+
+add_lock_button = ttk.Button(locked_player_frame, text='Add Player', command=add_locked_player_click, state='disabled')
+add_lock_button.grid(column=0, row=2, sticky='W')
+
+add_exclude_button = ttk.Button(excluded_player_frame, text='Add Player', command=add_excluded_player_click, state='disabled')
+add_exclude_button.grid(column=0, row=2, sticky='W')
+
+remove_lock_button = ttk.Button(locked_player_frame, text='Remove Player', command=remove_locked_player_click, state='disabled')
+remove_lock_button.grid(column=1, row=2, sticky='E')
+
+remove_exclude_button = ttk.Button(excluded_player_frame, text='Remove Player', command=remove_excluded_player_click, state='disabled')
+remove_exclude_button.grid(column=1, row=2, sticky='E')
 
 # Progressbar
 progress_bar = ttk.Progressbar(bottom_frame, orient='horizontal', mode='determinate')
