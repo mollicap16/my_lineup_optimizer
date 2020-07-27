23b1b17bb75fad66317718250da604008b7cce09
mollicap16 <mollicap16@yahoo.com>
2020-07-26 22:17:23 -0400

    Updated line_optimizer_gui.py test.

--- lineup_optimizer/test/lineup_optimizer_gui.py
+++ lineup_optimizer/test/lineup_optimizer_gui.py
@@ -25,6 +25,13 @@ class MyFirstGui:
 
 class LineupOptimizerGui():
     def __init__(self, master):
+        self.player_file=""
+        self.optimizer=get_optimizer(Site.DRAFTKINGS, Sport.FOOTBALL)
+        self.locked_player_list=[]
+        self.excluded_player_list=[]
+        self.player_value=[]
+        self.ffa_player_projections = pd.DataFrame()
+
         self.master = master 
         master.title("Lineup Optimizer")
 
@@ -111,24 +118,118 @@ class LineupOptimizerGui():
 
         master.config(menu=self.menubar)
 
-    #TODO: Finish details of commands
+    # Command Functions
     def load_click(self):
-        print("Clicked Load")
+        init_dir = ""
+        if (self.selected_sport.get() == 1):
+            init_dir = "/home/pete/Documents/dk_player_exports/"
+        elif (self.selected_sport.get() == 2):
+            init_dir = "/home/pete/Documents/nhl/dk_player_exports/"
+        else: 
+            sys.exit("INVALID SPORT SELECTED")
+        self.player_file = filedialog.askopenfilename(initialdir=init_dir, title="Selected Projections")
+        self.load_label.configure(text=self.player_file, width=0)
+        self.save_button.state(['disabled'])
+        self.optimize_button.state(['!disabled'])
+        try:
+            self.ffa_player_projections = pd.read_csv (self.player_file)
+            self.load_optimizer()
+            self.add_value_column()
+            self.ffa_player_projections = self.ffa_player_projections.assign(Value = self.player_value)
+        except:
+            sys.exit("Invalid file type")
+        self.table = Table(self.middle_left_frame, dataframe=self.ffa_player_projections)
+        self.table.show()
+        self.add_lock_button.state(['!disabled'])
+        self.add_exclude_button.state(['!disabled'])
+        self.remove_lock_button.state(['!disabled'])
+        self.remove_exclude_button.state(['!disabled'])
 
     def optimize_click(self):
+        self.lock_players()
+        self.exclude_players()
+        lineups = self.optimizer.optimize(int(self.num_lineups.get()))
+        self.progress_bar['value'] = 0
+        self.progress_bar['maximum'] = int(self.num_lineups.get())
         print("Clicked Optimize")
 
     def save_click(self):
         print("Clicked Save")
     
     def add_locked_player_click(self):
+        row = self.table.getSelectedRow()
+        player = self.ffa_player_projections.iloc[row].Name
+        if (player not in self.locked_player_list and player not in self.excluded_player_list):
+            self.locked_player_list.append(player)
+        print(player)
         print("Add Locked Player")
+        print(self.locked_player_list)
 
-    def add_excluded_player_click(self):
-        print("Add Excluded Player")
+        self.locked_player_listbox.delete(0, 'end')
+        for item in self.locked_player_list:
+            self.locked_player_listbox.insert('end', item)
 
     def remove_locked_player_click(self):
+        player = self.locked_player_listbox.get('active')
+        if (player in self.locked_player_list):
+            self.locked_player_list.remove(player)
+
+        self.locked_player_listbox.delete(0, 'end')
+        for item in self.locked_player_list:
+            self.locked_player_listbox.insert('end', item)
+        print(self.locked_player_list)
         print("Remove locked Player")
 
+    def add_excluded_player_click(self):
+        row = self.table.getSelectedRow()
+        player = self.ffa_player_projections.iloc[row].Name
+        if (player not in self.excluded_player_list and player not in self.locked_player_list):
+            self.excluded_player_list.append(player)
+
+        self.excluded_player_listbox.delete(0, 'end')
+        for item in self.excluded_player_list:
+            self.excluded_player_listbox.insert('end', item)
+        print(self.excluded_player_list)
+        print("Add Excluded Player")
+
+
     def remove_excluded_player_click(self):
+        player = self.excluded_player_listbox.get('active')
+        if (player in self.excluded_player_list):
+            self.excluded_player_list.remove(player)
+
+        self.excluded_player_listbox.delete(0, 'end')
+        for item in self.excluded_player_list:
+            self.excluded_player_listbox.insert('end', item)
         print("Removed Excluded Player")
+
+    # Helper Functions
+    def load_optimizer(self):
+        if (self.selected_sport.get() == 1):
+            self.optimizer = get_optimizer(Site.DRAFTKINGS, Sport.FOOTBALL)
+        elif (self.selected_sport.get() == 2):
+            self.optimizer = get_optimizer(Site.DRAFTKINGS, Sport.HOCKEY)
+        try:
+            self.optimizer.load_players_from_csv(self.player_file)
+        except:
+            sys.exit("Invalid csv file format")
+
+    def add_value_column(self):
+        self.player_value = []
+        players = self.optimizer.players
+        for player in players:
+            self.player_value.append(player.efficiency*1000)
+    
+    def lock_players(self):
+        for player_name in self.locked_player_list:
+            player = self.optimizer.get_player_by_name(player_name)
+            self.optimizer.add_player_to_lineup(player)
+            print(player)
+        print("lock_players()")
+
+    def exclude_players(self):
+        for player_name in self.excluded_player_list:
+            player = self.optimizer.get_player_by_name(player_name)
+            self.optimizer.remove_player(player)
+            print(player)
+        print("exclude_players")
